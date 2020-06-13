889f99aa4521e9599f3139f585d42e146e19788f
FaggottosMaximus <ali.dindamal@gmail.com>
2020-06-13 22:38:06 +0430

    hex to binary completed, parts are now separated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk 
+import math 
 
 def penalty_time():
     pass
@@ -6,14 +7,42 @@ def penalty_time():
 def access_time():
     pass
 
+def hex_to_binary(hex, mem_size, kb_or_mb):
+    if kb_or_mb == 'kb':
+        size = math.log(mem_size * 1024, 2)
+    else:
+        size = math.log(mem_size * 1024 * 1024, 2)
+    return str(bin(int(hex[2:], 16))[2:].zfill(int(size)))
+
+def tag_index_offset(binary, block_size, cache_size, cache_kb_or_mb):
+    full_length = len(binary)
+    if cache_kb_or_mb == 'kb':
+        cache_size = cache_size * 1024
+    else:
+        cache_size = cache_size * 1024 * 1024
+    offset_length = math.log(block_size, 2)
+    index_length = math.log(cache_size/block_size, 2)
+    tag_length = full_length - (offset_length + index_length)
+    return [int(tag_length), int(index_length), int(offset_length)]
+
 def main():
+    def step_pressed():
+        binary = hex_to_binary(entry_list['hex address'].get(), int(entry_list['memory size:'].get()), mem_var)
+        tag_index_offset_list = tag_index_offset(binary, int(entry_list['block size:'].get()), int(entry_list['cache size:'].get()), cache_var.get())
+        tag = binary[0:tag_index_offset_list[0]]
+        index = binary[tag_index_offset_list[0]:tag_index_offset_list[0] + tag_index_offset_list[1]]
+        offset = binary[tag_index_offset_list[0] + tag_index_offset_list[1]: tag_index_offset_list[0] + tag_index_offset_list[1] + tag_index_offset_list[2]]
+        print(binary,"\n",tag,index,offset)
+
+    def cacl_pressed():
+        pass
+
     window = tk.Tk()
     window.title("Direct mapping simulator")
     window.geometry("600x600")
     window.resizable(False, False)
-    
     btn_calc = tk.Button(master = window, text = "calculate", command = None, highlightbackground='black')
-    btn_step = tk.Button(master = window, text = 'step', command = None, highlightbackground='black')
+    btn_step = tk.Button(master = window, text = 'step', command = step_pressed, highlightbackground='black')
    
     labels_names = ['memory size:', 'cache size:', 'block size:', 'hex address', 'cache access time:', 'cache miss penalty time:', 'binary address:']
     entry_list = {}
